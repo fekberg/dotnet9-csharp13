4ef8977bcefa8431f348e4c6680aa8bfffcc5cbf
Filip Ekberg <mail@filipekberg.se>
2024-10-10 21:40:18 +0200

    Add samples

---
+++ dotnet/FeatureSwitches/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+var service = new UserService();
+
+service.Create("filip@ekberg.dev");
+
+public class UserService
+{
+    [FeatureSwitchDefinition("UserService.IsEnabled")]
+    private static bool IsEnabled => AppContext.TryGetSwitch("UserService.IsEnabled", out bool isEnabled) ? isEnabled : true;
+
+    public void Create(string email)
+    {
+        if(IsEnabled)
+        {
+            Console.WriteLine("A");
+        }
+    }
+}
